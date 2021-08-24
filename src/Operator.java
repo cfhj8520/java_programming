d951149ddf186b53be0ca5a260c603ed46cda3f9
cfhj8520 <cfhj8520@naver.com>
2021-08-24 21:04:06 +0900

    commit test

--- src/Operator.java
+++ src/Operator.java
@@ -10,18 +10,22 @@ public class Operator {
     public static void main(String[] args){
         int[] A = {1,2,3,4,5};
         int[] B = A;
+        int[] C = A.clone();
 
         System.out.println(Arrays.toString(A));		//[1, 2, 3, 4, 5]
         System.out.println(Arrays.toString(B));		//[1, 2, 3, 4, 5]
+        System.out.println(Arrays.toString(C));     //[1, 2, 3, 4, 5]
 
         A[0] = 10;
 
         System.out.println(Arrays.toString(A));		//[10, 2, 3, 4, 5]
         System.out.println(Arrays.toString(B));		//[10, 2, 3, 4, 5]
+        System.out.println(Arrays.toString(C));     //[1, 2, 3, 4, 5]
 
         add(A);
 
         System.out.println(Arrays.toString(A));		//[11, 3, 4, 5, 6]
         System.out.println(Arrays.toString(B));		//[11, 3, 4, 5, 6]
+        System.out.println(Arrays.toString(C));     //[1, 2, 3, 4, 5]
     }
 }
